16b39f40c0e1cc516b200b7517453d9f9b4b409b
dan-jel <daniel.heese1@gmail.com>
2020-02-20 20:30:33 +0100

    björn-remastered

---
+++ björn_remastered.py
@@ -0,0 +1,149 @@
+import googlemaps
+import xlsxwriter
+import os
+
+
+apikeyfile=open("apikey.txt","r")
+apikey=apikeyfile.read()
+gmaps=googlemaps.Client(key=apikey)
+
+
+def wget(name):
+    a="""wget --post-file=./xml/""" + name + """.xml --header="Content-Type: text/xml" -O ./xml/out-""" + name + ".xml https://efa-bw.de/trias"""
+    os.system(a)
+    return
+
+
+def maps(ort):
+    url=str(gmaps.geocode(ort))
+    anfang=url.find("location")+19
+    ende=url[anfang:].find(",")+anfang
+    lat=url[anfang:ende]
+    anfang=anfang+len(lat)+9
+    ende=url[anfang:].find("}")+anfang
+    long=url[anfang:ende]
+    return([ort,lat,long])
+
+
+def xml(name,slat,slong,zlat,zlong):
+    name="./xml/" + name + ".xml"
+    file = open(name, "w")
+    file.write("""<?xml version="1.0" encoding="UTF-8"?>""" + "\n")
+    file.write("""<Trias version="1.1" xmlns="http://www.vdv.de/trias" xmlns:siri="http://www.siri.org.uk/siri" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.vdv.de/trias file:///C:/development/HEAD/extras/TRIAS/TRIAS_1.1/Trias.xsd">""" + "\n")
+    file.write(" <ServiceRequest>" + "\n")
+    file.write("   <siri:RequestTimestamp>2016-08-11T11:00:00</siri:RequestTimestamp>" + "\n")
+    file.write("   <siri:RequestorRef>test</siri:RequestorRef>" + "\n")
+    file.write("   <RequestPayload>" + "\n")
+    file.write("    <TripRequest>" + "\n")
+    file.write("     <Origin>" + "\n")
+    file.write("      <LocationRef>" + "\n")
+    file.write("       <GeoPosition>" + "\n")
+    file.write("        <Longitude>" + slong + "</Longitude>" + "\n")
+    file.write("        <Latitude>" + slat + "</Latitude>" + "\n")
+    file.write("       </GeoPosition>" + "\n")
+    file.write("      </LocationRef>" + "\n")
+    file.write("     </Origin>" + "\n")
+    file.write("     <Destination>" + "\n")
+    file.write("      <LocationRef>" + "\n")
+    file.write("       <GeoPosition>" + "\n")
+    file.write("        <Longitude>" + zlong + "</Longitude>" + "\n")
+    file.write("        <Latitude>" + zlat + "</Latitude>" + "\n")
+    file.write("       </GeoPosition>" + "\n")
+    file.write("      </LocationRef>" + "\n")
+    file.write("     </Destination>" + "\n")
+    file.write("    </TripRequest>" + "\n")
+    file.write("   </RequestPayload>" + "\n")
+    file.write(" </ServiceRequest>" + "\n")
+    file.write("</Trias>" + "\n")
+    file.close()
+    return
+
+
+def kordsout(alle):
+    kordsbook = xlsxwriter.Workbook("lat-long-out.xlsx")
+    sheetk= kordsbook.add_worksheet()
+    sheetk.write("A1" , "Stadt")
+    sheetk.write("B1" , "Lat")
+    sheetk.write("C1" , "Long")
+    zeile=2
+    for eine in alle:
+        x="A" + str(zeile)
+        y=eine[0]
+        sheetk.write(x,y)
+
+        x="B" + str(zeile)
+        y=eine[1]
+        sheetk.write(x,y)
+
+        x="C" + str(zeile)
+        y=eine[2]
+        sheetk.write(x, y)
+
+        zeile += 1
+    kordsbook.close()
+    print("Fertig!")
+    os.system("start out.xlsx")
+    return
+
+
+def routsout(start,ziel):
+    name=start[0] + "-" + ziel[0]
+    slat=start[1]
+    slong=start[2]
+    zlat=ziel[1]
+    zlong=ziel[2]
+    xml(name,slat,slong,zlat,zlong)
+    wget(name)
+    return
+
+
+def kords(infos):
+    sammeln=[]
+    for i in infos:
+        info=maps(i)
+        sammeln.append(info)
+    kordsout(sammeln)
+    return
+
+
+def routs(routen):
+    commands=[]
+    for route in routen:
+        route=route.split("-")
+        start=route[0]
+        ziel=route[1]
+        start=maps(start)
+        ziel=maps(ziel)
+        com=routsout(start,ziel)
+    return
+
+
+def kordsgui():
+    print("Bitte gib die gewünschten Städte mit einem Komma getrennt ein:")
+    citys=str(input("--> "))
+    citys=citys.split(",")
+    kords(citys)
+    return
+
+
+def routsgui():
+    print("Bitte gib die Routen wie folge ein: Start-Ziel,Start-Ziel,Start-Ziel ")
+    routen=input("--> ")
+    routen=routen.split(",")
+    routs(routen)
+    return
+
+
+print("Möchtest du Orte in Koordinaten umrechnen (1) oder eine Route berechnen (2) ?")
+
+z=False
+while z == False:
+    eingabe=int(input("--> "))
+
+    if eingabe == 1:
+        kordsgui()
+        z=True
+
+    elif eingabe == 2:
+        routsgui()
+        z=True
